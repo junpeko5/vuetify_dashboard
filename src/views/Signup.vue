f57e86f3962ac88b27c5d5ce920ec465d0551ad3
junpeko5 <junpeko5@gmail.com>
2022-01-04 23:15:49 +0900

    Forms: Validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,8 +3,14 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form>
-          <v-text-field label="Email" type="email">
+        <v-form ref="signUpForm" v-model="formValidity">
+          <v-text-field
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+            required
+          >
           </v-text-field>
           <v-autocomplete
             label="Which browser do you use?"
@@ -23,11 +29,37 @@
           ></v-date-picker>
           <v-checkbox
             label="Agree to terms & conditions"
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
           ></v-checkbox>
           <v-btn
             type="submit"
             color="primary"
-          ></v-btn>
+            class="mr-4"
+            :disabled="!formValidity"
+          >
+            Submit
+          </v-btn>
+          <v-btn
+            class="mr-4"
+            color="success"
+            @click="validateForm"
+          >
+            Validate Form
+          </v-btn>
+          <v-btn
+            color="warning"
+            @click="resetValidation"
+            class="mr-4"
+          >
+            Reset Validation
+          </v-btn>
+          <v-btn
+            color="error"
+            @click="resetForm"
+          >
+            Reset
+          </v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -46,7 +78,30 @@ export default {
         'Edge',
         'Brave',
       ],
-
+      agreeToTerms: false,
+      agreeToTermsRules: [
+        value => !!value || 'You must agree to the terms and conditions to sign up for an account.',
+      ],
+      email: '',
+      emailRules: [
+        value => !!value || 'Email is required.',
+        value => value.indexOf('@') !== 0 || 'Email should have a username.',
+        value => value.includes('@') || 'Email should include an @ symbol',
+        value => value.indexOf('.') - value.indexOf('@') > 1 || 'Email should contain a valid domain.',
+        value => value.indexOf('.') <= value.length - 3 || 'Email should contain a valid domain extension.'
+      ],
+      formValidity: false,
+    }
+  },
+  methods: {
+    resetForm() {
+      this.$refs.signUpForm.reset()
+    },
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation()
+    },
+    validateForm() {
+      this.$refs.signUpForm.validate()
     }
   }
 }
